6ca1dda93a613d1b8425626a6e3c93ec924793af
JyotiKM29 <jyotikumari4442@gmail.com>
2023-11-25 10:32:45 +0530

    hover menu added

--- app/component/Navbar.jsx
+++ app/component/Navbar.jsx
@@ -13,6 +13,8 @@ import { RxCross1 } from "react-icons/rx";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const[isBrands , setBrands] = useState(false)
+  const[isShop , setShop] = useState(false)
   const [isWidthSmallerThan1024, setIsWidthSmallerThan1024] = useState(false);
   const [isWidthSmallerThan700, setIsWidthSmallerThan700] = useState(false);
 
@@ -29,6 +31,22 @@ function Navbar() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  const handleHoverBrand = () => {
+    setBrands(true);
+  };
+
+  const handleLeaveBrand = () => {
+    setBrands(false);
+  };
+
+  const handleHoverShop = () => {
+    setShop(true);
+  };
+
+  const handleLeaveShop = () => {
+    setShop(false);
+  };
 
   return (
     <>
@@ -49,14 +67,57 @@ function Navbar() {
         </>
       ) : (
         <div className="flex gap-8">
-          <h4 className="hover flex items-center gap-1">
+        <div className="relative">
+          <button className="hover flex items-center gap-1"
+          onMouseEnter={handleHoverBrand}
+      onMouseLeave={handleLeaveBrand}
+          >
             brands
             <IoIosArrowDown className="w-3 h-3" />
-          </h4>
-          <h4 className="hover flex items-center gap-1">
+          </button>
+          {isBrands && <div className="lineUp  absolute flex flex-col items-start h-auto w-auto p-6 gap-1 bg-[#2c2c2c] mt-2">
+            <button className="hover">all brands</button>
+            <button className="hover">shosky</button>
+            <button className="hover">bitfins</button>
+            <button className="hover whitespace-nowrap">cardano stonez club</button>
+            <button className="hover">cryptoraggies</button>
+            <button className="hover">danketsu</button>
+            <button className="hover">future fest</button>
+            <button className="hover">pendulum</button>
+            <button className="hover">vudu brigada</button>
+          </div>}</div>
+
+          <div className="relative">
+          <button className="hover flex items-center gap-1"
+            onMouseEnter={handleHoverShop}
+      onMouseLeave={handleLeaveShop}>
             shop
             <IoIosArrowDown className="w-3 h-3" />
-          </h4>
+          </button>
+          {isShop && <div className="lineUp  absolute flex justify-between h-auto w-screen px-12 py-6 gap-1 bg-[#2c2c2c] mt-6" 
+          style={{right: '-88.5vw'}}
+          >
+            <ul className="flex flex-col items-start">
+            <button className="hover text-2xl">apperal</button>
+            <button className="hover">unisex hoodies</button>
+            <button className="hover">unisex zip hoodies</button>
+            <button className="hover">unisex sweatshirt</button>
+            <button className="hover">unisex tees</button>
+            <button className="hover">womens tank</button>
+            <button className="hover">unisex tanks</button>
+            
+            </ul>
+            <ul className="flex flex-col items-start">
+            <button className="hover text-2xl">home</button>
+            <button className="hover">phone case</button>
+            <button className="hover">framed posters</button>
+            <button className="hover">posters</button>
+           
+            </ul>
+
+          </div>}
+          </div>
+        
 
           <h4 className="hover">premium</h4>
           <h4 className="hover">gift cards</h4>
